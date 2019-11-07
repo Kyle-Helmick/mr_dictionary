973d816a44064db6ca774c0d248358c9138641b5
Kyle-Helmick <helmick.kyle@gmail.com>
2019-11-07 03:07:01 -0800

    🚧 (Rewriting our beloved friend): Rewriting our beloved friend

--- mr_dictionary.py
+++ mr_dictionary.py
@@ -1,71 +1,168 @@
-import re
-import time
-import requests
 import json
+import linecache
+import logging
+import re
+import os
+import random
 import socket
-from slackclient import SlackClient
+import time
 
-FILE = open("/home/kyle/mr_dictionary/secret.json", "r")
-STRING_SECRETS = FILE.read()
-SECRETS = json.loads(STRING_SECRETS)
+import requests
+import slack
+
+logging.basicConfig(
+    format='%(asctime)s [MrDictionary] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'
+)
+
+FILE = open('./config.json', 'r')
+CONFIG = json.loads(FILE.read())
 FILE.close()
 
-print("STARTUP")
+DICTIONARY_FILE = 'dictionary.txt'
+
+REQUIRED_CONFIGS = set(
+    ['slack_token', 'slack_verification', 'dictionary_key', 'thesaurus_key']
+)
+loaded_configs = set(CONFIG.keys())
+
+try:
+    assert loaded_configs.issubset(REQUIRED_CONFIGS)
+    logging.info('Loaded config')
+except Exception as e:
+    logging.error(
+        f'Required keys [{REQUIRED_CONFIGS.difference(loaded_configs)}] not found in config.')
+    pass
 
 LANGUAGE = 'en'
 
-slack_client = SlackClient(SECRETS['slack_key'])
 
-user_list = slack_client.api_call("users.list")
+def download_file(url):
+    if os.path.exists(DICTIONARY_FILE):
+        return
 
-for user in user_list.get('members'):
-    if user.get('name') == "mr_dictionary":
-        slack_user_id = user.get('id')
-        break
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(DICTIONARY_FILE, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
 
-if slack_client.rtm_connect():
-    print("Connected!")
-    while True:
-        for message in slack_client.rtm_read():
-
-            if 'text' in message and re.findall(r"([Dd]efine: |[Ww]hat is |[Ww]hat\'s |[Ww]hats )([^?^.^!].+?)([.?!]+|$)", message['text']):
-
-                message_text = re.findall(r"([Dd]efine: |[Ww]hat is |[Ww]hat\'s |[Ww]hats )([^?^.^!].+?)([.?!]+|$)", message['text'])
-
-                print("Message captured: ", message_text)
-
-                word = message_text[0][len(message_text[0])-2]
-
-                print("Word identified: ", word)
-                
-                error = 1
-
-                while error:
-                    random_word_url = "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=0&maxCorpusCount=0&minDictionaryCount=3&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key="+SECRETS['wordnik_key']
-                    
-                    response = requests.get(random_word_url)
-                    random_word = response.json()['word']
-                    print("random_word: ", random_word)
-
-                    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + LANGUAGE + '/' + random_word.lower()
-
-                    response = requests.get(url, headers={'app_id': SECRETS['oxford_id'], 'app_key': SECRETS['oxford_key']})
-
-                    try:
-                        response = response.json()
-                        definition = response['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
-                        error = 0
-                        formatted_response = "The definition of “{0}” is: {1}".format(word, definition)
-                        print("Formatted response: ", formatted_response)
-                    except:
-                        print("need to get a new word")
-                        error = 1
+    return DICTIONARY_FILE
 
 
-                slack_client.api_call(
-                    "chat.postMessage",
-                    channel=message['channel'],
-                    text=formatted_response,
-                    as_user=True)
+def get_random_word():
+    random.seed()
+    rand_word_index = random.randint(0, 81882)
+    rand_word = linecache.getline(DICTIONARY_FILE, rand_word_index)
+    if '%' in rand_word:
+        rand_word = get_random_word()
+    return rand_word.strip()
 
-        time.sleep(.5)
+
+def define_word(word):
+    url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={CONFIG['dictionary_key']}'
+    response = requests.get(url)
+    defs = [x.capitalize() for x in response.json()[0]['shortdef']]
+    return defs
+
+
+def formatted_response(random_word, definitions):
+    if len(definitions == 1):
+        return f'{random_word.capitalize()} might mean the following!\n{definitions[0]}'
+
+    return f"{random_word.capitalize()} might mean one of the following!\n{' | '.join(definitions)}"
+
+
+def lambda_handler(event, context):
+
+    body = json.loads(event['body'])
+
+    if 'challenge' in body:
+        return {
+            'statusCode': 200,
+            'body': body['challenge']
+        }
+
+    if body['token'] != CONFIG['slack_token']:
+        return {
+            'statusCode': 401,
+            'body': 'Invalid signing'
+        }
+
+    client = slack.WebClient(token=CONFIG['slack_token'])
+
+    user_list = client.users_list()
+
+    mr_dictionary = list(filter(
+        lambda user: user['is_bot'] and not user['deleted'] and 'dict' in user['name'],
+        user_list['members']
+    ))
+
+    if body['event']['user'] == mr_dictionary['id']:
+        return {
+            'statusCode': 200,
+            'body': 'no-op'
+        }
+
+    if 'text' in body['event'] and re.findall(r'([Dd]efine: |[Ww]hat is |[Ww]hat\'s |[Ww]hats )([^?^.^!].+?)([.?!]+|$)', body['event']['text']):
+        message_text = re.findall(
+            r'([Dd]efine: |[Ww]hat is |[Ww]hat\'s |[Ww]hats )([^?^.^!].+?)([.?!]+|$)', body['event']['text'])
+
+        logging.info(f'Message captured: {message_text}')
+
+        word = message_text[0][len(message_text[0])-2]
+
+        logging.info(f'Word identified: {word}')
+
+        dictionary_tries = 3
+
+        while dictionary_tries:
+            try:
+                download_file(
+                    'https://gist.githubusercontent.com/Kyle-Helmick/11ca009418c600c946a9a0de7a0987ba/raw/ff2cac9bce8a621c29c146b00d96b2db21b78be6/2of12inf.txt'
+                )
+                break
+            except:
+                logging.error('dictionary failed to download')
+                dictionary_tries -= 1
+                if dictionary_tries == 0:
+                    return {
+                        'statusCode': 500,
+                        'body': f'{DICTIONARY_FILE} failed to download'
+                    }
+
+        get_word_tries = 3
+
+        while get_word_tries:
+            random_word = get_random_word()
+            logging.info(f'random_word: {random_word}')
+
+            try:
+                definitions = define_word(random_word)
+                if len(definitions) == 0:
+                    raise Exception(
+                        f'{random_word} did not have any definitions'
+                    )
+                break
+            except:
+                logging.error('Failed to pick word with definitions')
+                get_word_tries -= 1
+                if get_word_tries == 0:
+                    return {
+                        'statusCode': 500,
+                        'body': 'Continuously failed to pick word with definitions'
+                    }
+
+        response_body = formatted_response(word, definitions)
+
+        channel = body['event']['channel']
+
+        client.chat_postMessage(
+            channel=channel,
+            text=response_body
+        )
+
+        return {
+            'statusCode': 200,
+            'body': "Successful chat response"
+        }
